61863735db27234df222a0a82159d1e1f9653402
Nick Phillips <darlenedolly@comcast.net>
2016-04-16 21:55:19 +0000

    chapter 7 exercises

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,13 +7,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post_via_redirect users_path, user: {name: "nick", email: "me@user.com", password: "choppy", password_confirmation: "choppy"}
     end
     assert_template 'users/show'
+    assert_select 'div.alert-success'
   end
   
   test "unsuccessful user sign up" do 
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, user: {name: "nick", email: "me@user.com", password: "choppy", password_confirmation: "choppier"}
+      post users_path, user: {name: "", email: "me@user.com", password: "choppy", password_confirmation: "choppier"}
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
 end
